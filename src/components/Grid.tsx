55c336e2864f46be3e8339a2d5578ef5baff8349
illavarasi <illavarasi@mercu.io>
2025-09-11 22:37:48 +0530

    create form send to git

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -3,60 +3,67 @@ import { useNavigate } from "react-router-dom";
 import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import { useDispatch } from "react-redux";
 
-import { getUsers } from "../data/http.service";
+import { deleteUser, getUsers } from "../data/http.service";
 import type { Users } from "../shared/models/User.model";
-import  {setUserData} from "../shared/slices/UserSlice"
+import { setUserData } from "../shared/slices/UserSlice"
 
 const Grid = () => {
-    const navigate=useNavigate()
+    const navigate = useNavigate()
     const [usersData, setUsersData] = useState<Users[]>([]);
-    const dispatch=useDispatch();
+    const dispatch = useDispatch();
     useEffect(() => {
         getUsers().then((data) => setUsersData(data));
     }, []);
-    const onViewClick=(user:Users)=>{
+    const onViewClick = (user: Users) => {
         dispatch(setUserData(user))
-navigate('/view');
+        navigate('/view');
+    }
+    const onDeleteClick = async (user: Users) => {
+        await deleteUser(user.id)
+    }
+    const onUpdateClick = (user: Users) => {
+
     }
     return (
         <>
-        <section className="flex justify-center items-center">
-  <TableContainer component={Paper} className="border border-blue-400 rounded-md p-5 m-5 w-[90%]">
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align="center">Name</TableCell>
-                            <TableCell align="center">Email</TableCell>
-                            <TableCell align="center">Phone</TableCell>
-                            <TableCell align="center">Website</TableCell>
-                            <TableCell align="center">Actions</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {
-                            usersData.map((data, index) => {
-                                return (
-                                    <TableRow key={index}>
-                                        <TableCell align="center">{data.name}</TableCell>
-                                        <TableCell align="center">{data.email}</TableCell>
-                                        <TableCell align="center">{data.phone}</TableCell>
-                                        <TableCell align="center">{data.website}</TableCell>
-                                        <TableCell className="flex flex-row justify-center items-center gap-5">
-                                        <Button variant="outlined" onClick={()=>onViewClick(data)}>view</Button>
-                                        <Button variant="outlined">Update</Button>
-                                        <Button variant="outlined">Delete </Button>
-                                        </TableCell>
+            <section className="flex flex-col justify-center items-start p-10">
+                <Button variant="outlined" onClick={()=>navigate("/create")}>CreateUser </Button>
+                <TableContainer component={Paper} className="border border-blue-400 rounded-md p-5 m-5 w-[90%]">
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="center">Name</TableCell>
+                                <TableCell align="center">Email</TableCell>
+                                <TableCell align="center">Phone</TableCell>
+                                <TableCell align="center">Website</TableCell>
+                                <TableCell align="center">Actions</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                usersData.map((data, index) => {
+                                    return (
+                                        <TableRow key={index}>
+                                            <TableCell align="center">{data.name}</TableCell>
+                                            <TableCell align="center">{data.email}</TableCell>
+                                            <TableCell align="center">{data.phone}</TableCell>
+                                            <TableCell align="center">{data.website}</TableCell>
+                                            <TableCell className="flex flex-row justify-center items-center gap-5">
+                                                <Button variant="outlined" onClick={() => onViewClick(data)}>view</Button>
+                                                <Button variant="outlined" onClick={() => onUpdateClick(data)}>Update</Button>
+                                                <Button variant="outlined" onClick={() => onDeleteClick(data)}>Delete </Button>
+                                            </TableCell>
 
-                                    </TableRow>
-                                )
-                            })
-                        }
+                                        </TableRow>
+                                    )
+                                })
+                            }
 
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </section>
-          
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </section>
+
         </>
     );
 };
